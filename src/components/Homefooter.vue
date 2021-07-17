3d25ccd44a4234ec2c84a9b64d527ecfa0817c41
HuangYiSheng <674845142@qq.com>
2021-07-17 13:03:23 +0800

    homefooter 未完成

--- src/components/Homefooter.vue
+++ src/components/Homefooter.vue
@@ -1,112 +1,100 @@
 <template>
   <footer>
-    <div class="wave-wrap">
-      <svg
-        version="1.1"
-        id="wave"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 119 26"
-      >
-        <path
-          class="path"
-          d="M120.8,26C98.1,26,86.4,0,60.4,0C35.9,0,21.1,26,0.5,26H120.8z"
-        ></path>
-      </svg>
-    </div>
-    <ul class="list-wrap">
-      <li data-color="#eccc68" title="Home">
-        <!-- <img src="./1.png" alt="" /> -->
-      </li>
-      <li data-color="#ff6b81" title="Profile">
-        <!-- <img src="./1.png" alt="" /> -->
-      </li>
-      <li data-color="#7bed9f" title="Get a beer!">
-        <!-- <img src="./1.png" alt="" /> -->
-      </li>
-      <li data-color="#70a1ff" title="Files">
-        <!-- <img src="./1.png" alt="" /> -->
-      </li>
-      <li data-color="#dfe4ea" title="Settings">
-        <!-- <img src="./1.png" alt="" /> -->
-      </li>
-    </ul>
+    <van-tabbar v-model="active">
+      <van-tabbar-item>
+        <span>自定义</span>
+        <template #icon="props">
+          <van-image
+            height:100%
+            :src="props.active ? icon.active : icon.inactive"
+          />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </footer>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
-  data() {
-    return {};
+  setup() {
+    const active = ref(0);
+    const icon = {
+      active: require("../assets/images/home/index_b.png"),
+      inactive: require("../assets/images/home/index_s.png"),
+    };
+    return {
+      icon,
+      active,
+    };
   },
-
-  components: {},
-
-  computed: {},
-
-  mounted() {},
-
-  methods: {},
 };
 </script>
 
-<style lang="less" scoped>
-footer {
-  width: 440px;
+<style lang="less">
+.van-tabbar {
+  height: 49px;
 }
+// footer {
+//   width: 375px;
+//   margin-bottom: 34px;
+//   background: #000;
+//   position: absolute;
+//   bottom: 0;
+//   // right: 0;
+//   // left: 0;
+//   .wave-wrap {
+//     position: relative;
+//     width: 375px;
+//     height: 16px;
+//     overflow: hidden;
+//     // margin-bottom: 0;
+//     #wave {
+//       position: absolute;
+//       width: 150px;
+//       transform-origin: bottom;
+//       transform: scaleY(0.8);
+//       transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+//       .path {
+//         fill: #f8f8f8;
+//       }
+//     }
+//   }
+//   .list-wrap {
+//     width: 375px;
+//     height: 49px;
+//     display: flex;
+//     justify-content: space-around;
+//     background: #f8f8f8;
+//     padding: 0 20px;
+//     li {
+//       cursor: pointer;
+//       position: relative;
+//       background: blue;
+//       width: 100%;
+//       height: 100%;
+//       display: flex;
+//       justify-content: center;
+//       align-items: center;
+//       transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+//     }
+//     li.active {
+//       margin-top: -10px;
+//       background: red;
+//     }
+//     li.active:before {
+//       transform: scale(1);
+//     }
+//   }
+// }
 
-.wave-wrap {
-  position: relative;
-  width: 100%;
-  height: 33px;
-  overflow: hidden;
-  margin-bottom: 0;
-}
-
-.wave-wrap #wave {
-  position: absolute;
-  width: 150px;
-  transform-origin: bottom;
-  transform: scaleY(0.8);
-  transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
-}
-
-.wave-wrap #wave .path {
-  fill: #2f3542;
-}
-
-.list-wrap {
-  display: flex;
-  width: 100%;
-  height: 80px;
-  background: #2f3542;
-  list-style: none;
-  justify-content: space-around;
-  padding: 0 20px;
-}
-
-.list-wrap li {
-  cursor: pointer;
-  position: relative;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
-}
-
-.list-wrap li.active {
-  margin-top: -10px;
-}
-
-.list-wrap li.active:before {
-  transform: scale(1);
-}
-
-img {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
+// img {
+//   width: 50px;
+//   height: 50px;
+//   border-radius: 50%;
+// }
 </style>
--- src/main.js
+++ src/main.js
@@ -10,36 +10,56 @@ import "./assets/css/reset.less";
 
 // 按需引入vant组件
 import {
+  // 基础组件
   Toast, // 轻提示
   Icon, // 图标
   Image as VanImage, // 图片
-  NavBar, // 导航
-  ShareSheet, // 分享面板
+
+  // 反馈组件
   ActionSheet, // 动作面板
-  Tab, // tap切换
-  Tabs, // tap切换
-  Card, // 商品卡片
+  ShareSheet, // 分享面板
+
+  // 展示组件
+  Lazyload, //懒加载
+
+  // 导航组件
   ActionBar, // 动作栏
   ActionBarIcon, // 动作栏
   ActionBarButton, // 动作栏
-  Grid,
-  GridItem,
+  NavBar, // 导航
+  Tab, // tap切换, 标签页
+  Tabs, // tap切换, 标签页
+  Tabbar, //标签栏
+  TabbarItem, //标签栏
+  Grid, //宫格
+  GridItem, //宫格
+
+  // 业务组件
+  Card, // 商品卡片
 } from "vant";
 
 const app = createApp(App);
+
+// 基础组件
 app.use(Toast); // 轻提示
 app.use(Icon); // 图标
 app.use(VanImage); // 图片
-app.use(NavBar); // 导航
-app.use(ShareSheet); // 分享面板
+
+// 反馈组件
 app.use(ActionSheet); // 动作面板
-app.use(Tab); // tap切换
-app.use(Tabs); // tap切换
+app.use(ShareSheet); // 分享面板
+
+// 展示组件
+app.use(Lazyload); //懒加载
+
+// 导航组件
+app.use(ActionBar).use(ActionBarIcon).use(ActionBarButton); // 动作栏
+app.use(Grid).use(GridItem); //宫格
+app.use(NavBar); // 导航
+app.use(Tab).use(Tabs); // tap切换,标签页
+app.use(Tabbar).use(TabbarItem); //标签栏
+
+// 业务组件
 app.use(Card); // 商品卡片
-app.use(ActionBar); // 动作栏
-app.use(ActionBarIcon); // 动作栏
-app.use(ActionBarButton); // 动作栏
-app.use(Grid);
-app.use(GridItem);
 
 app.use(store).use(router).mount("#app");
