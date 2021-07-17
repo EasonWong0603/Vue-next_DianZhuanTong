323b1a452dab609b55532b00a33142275ac387de
ZhouBuHong <862360493@qq.com>
2021-07-17 09:15:36 +0800

    zbh-未完

--- src/main.js
+++ src/main.js
@@ -5,38 +5,37 @@ import App from "./App.vue";
 import router from "./router";
 //引入Vuex.store
 import store from "./store";
-
 //引入重置css样式
 import "./assets/css/reset.less";
 
 // 按需引入vant组件
 import {
-  Toast,
-  Icon,
-  Image as VanImage,
-  NavBar,
-  ShareSheet,
-  ActionSheet,
-  Tab,
-  Tabs,
-  Card,
-  ActionBar,
-  ActionBarIcon,
-  ActionBarButton,
+  Toast, // 轻提示
+  Icon, // 图标
+  Image as VanImage, // 图片
+  NavBar, // 导航
+  ShareSheet, // 分享面板
+  ActionSheet, // 动作面板
+  Tab, // tap切换
+  Tabs, // tap切换
+  Card, // 商品卡片
+  ActionBar, // 动作栏
+  ActionBarIcon, // 动作栏
+  ActionBarButton, // 动作栏
 } from "vant";
 
 const app = createApp(App);
-app.use(Toast);
-app.use(Icon);
-app.use(VanImage);
-app.use(NavBar);
-app.use(ShareSheet);
-app.use(ActionSheet);
-app.use(Tab);
-app.use(Tabs);
-app.use(Card);
-app.use(ActionBar);
-app.use(ActionBarIcon);
-app.use(ActionBarButton);
+app.use(Toast); // 轻提示
+app.use(Icon); // 图标
+app.use(VanImage); // 图片
+app.use(NavBar); // 导航
+app.use(ShareSheet); // 分享面板
+app.use(ActionSheet); // 动作面板
+app.use(Tab); // tap切换
+app.use(Tabs); // tap切换
+app.use(Card); // 商品卡片
+app.use(ActionBar); // 动作栏
+app.use(ActionBarIcon); // 动作栏
+app.use(ActionBarButton); // 动作栏
 
 app.use(store).use(router).mount("#app");
