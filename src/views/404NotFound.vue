ef362ce3de01b56cf7dc7e1b9c1ba7dadb978102
HuangYiSheng <674845142@qq.com>
2021-07-18 17:29:21 +0800

    404 css finish

--- src/views/404NotFound.vue
+++ src/views/404NotFound.vue
@@ -1,21 +1,24 @@
 <template>
-  <div>404</div>
+  <div>
+    <img
+      src="https://d1.faiusr.com/4/AAEIABAEGAAg_Y-o6wUog4_eATDABziaBA.png"
+      alt=""
+    />
+  </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {};
-  },
-
-  components: {},
-
-  computed: {},
-
-  mounted() {},
-
-  methods: {},
-};
-</script>
-
-<style lang="less" scoped></style>
+<style lang="less">
+// 引入自定义样式
+@import "../assets/css/var.less";
+html,
+body {
+  height: 100%;
+}
+div {
+  height: 100%;
+  .flex-center();
+  img {
+    width: 100%;
+  }
+}
+</style>
