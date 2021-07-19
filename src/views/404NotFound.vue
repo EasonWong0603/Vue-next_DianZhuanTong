45bd92291438e9f00f96b4a998e5974ffce42db1
HuangYiSheng <674845142@qq.com>
2021-07-19 08:52:28 +0800

    7.19 404

--- src/views/404NotFound.vue
+++ src/views/404NotFound.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="NotFound">
-    <img
-      src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.huabanimg.com%2F820debe8b6be1c8b143500696694685b9d17e26e48749-2x6FOf_fw658&refer=http%3A%2F%2Fhbimg.huabanimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1629198364&t=6c81303b777ea70f2332f650948630aa"
-    />
+    <img src="../assets/images/404page.png" />
   </div>
 </template>
 
@@ -18,11 +16,9 @@ body {
 #NotFound {
   height: 100%;
   .flex-center();
-  background: #7ebeed;
-
+  // background: orange;
   img {
     width: 100%;
-    // height: 100%;
   }
 }
 </style>
