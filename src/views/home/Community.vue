3f60d41931b798a1eda537287c48b0080df0d6b1
zhuke <826417763@qq.com>
2021-07-19 10:35:43 +0800

    19号

--- src/views/home/Community.vue
+++ src/views/home/Community.vue
@@ -29,7 +29,37 @@
         </li>
         <li class="chart3">+ 关注</li>
         <li class="chart4">
-          你还在纠结大盘不放量吗？其实你知道A股已经开始裂变了吗？美股化顿，港股化已是不争的事实，A股不需要全面上涨，全面放量，现在就是精准打击、精准放量阶段，只有集中力量把科技、金融撬动，才能有效刺激放量。多方位的刺激与地域性的差异导致.....
+          你还在纠结大盘不放量吗？其实你知道A股已经开始裂变了吗？美股化顿，港股化已是不争的事实，A股不需要全面上涨，全面放量，现在就是精准打击、精准放量阶段，只有集中力量把科技、金融撬动，才能有效刺激放量。多方位的刺激与地域性的差异导致全面放量现在就是精准打击
+        </li>
+        <li class="chart5"></li>
+        <li class="chart6">
+          <span>转发</span>
+          <span>1234</span>
+          <span>2345</span>
+        </li>
+      </ul>
+      <ul class="chart">
+        <li class="chart1"></li>
+        <li class="chart2">
+          <p>阳光的阳光</p>
+          <span>40分钟</span>
+        </li>
+        <li class="chart3">+ 关注</li>
+        <li class="chart4">
+          你还在纠结大盘不放量吗？其实你知道A股已经开始裂变了吗？美股化顿，港股化已是不争的事实，A股不需要全面上涨，全面放量，现在就是精准打击、精准放量阶段，只有集中力量把科技、金融撬动，才能有效刺激放量。多方位的刺激与地域性的差异导致全面放量现在就是精准打击
+        </li>
+        <li class="chart5">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </li>
+        <li class="chart6">
+          <span>转发</span>
+          <span>1234</span>
+          <span>2345</span>
         </li>
       </ul>
     </div>
@@ -124,6 +154,8 @@ export default {
       box-shadow: 0px 0px 9px 0px rgba(182, 182, 182, 0.42);
       border-radius: 17px 17px 0px 0px;
       padding: 13px 16px 43px 19px;
+      position: relative;
+      margin-bottom: 40px;
       li {
         display: block;
       }
@@ -139,7 +171,7 @@ export default {
         margin-top: 5px;
         float: left;
         p {
-          width: 32px;
+          width: 100px;
           height: 10px;
           font-size: 14px;
           font-family: PingFang;
@@ -170,6 +202,50 @@ export default {
         font-weight: 500;
         color: #ffffff;
         text-shadow: 0px 1px 0px rgba(248, 84, 27, 0.48);
+      }
+      .chart4 {
+        margin-top: 16px;
+        float: left;
+        width: 340px;
+        height: 98px;
+        font-size: 14px;
+        font-family: PingFang;
+        font-weight: 500;
+        color: #666666;
+        line-height: 20px;
+        display: -webkit-box; /*将对象转为弹性盒模型展示*/
+        -webkit-box-orient: vertical; /*设置弹性盒模型子元素的排列方式*/
+        -webkit-line-clamp: 5; /*限制文本行数*/
+        overflow: hidden; /*超出隐藏*/
+        span {
+          width: 29px;
+          height: 14px;
+          font-size: 15px;
+          font-family: PingFang;
+          font-weight: 500;
+          color: #ff564b;
+          line-height: 20px;
+        }
+      }
+      .chart5 {
+        width: 338px;
+        height: 147px;
+        background: #f8f8f8;
+        border-radius: 3px;
+        background: red;
+        float: left;
+        margin-top: 10px;
+      }
+      .chart6 {
+        display: flex;
+        position: absolute;
+        width: 336px;
+        height: 39px;
+        background: #fff;
+        box-shadow: 0px 0px 9px 0px rgba(203, 185, 177, 0.42);
+        border-radius: 3px;
+        bottom: -15px;
+        .flex-between();
       }
     }
   }
